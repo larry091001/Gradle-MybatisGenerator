55e038aee3123bddf1c76ea797584a78197d07a7
zhang_ying <zhang_ying@suixingpay.com>
2019-08-04 13:09:04 +0800

    Initial commit

---
+++ src/main/java/com/bean/UserInfo.java
@@ -0,0 +1,99 @@
+package com.bean;
+
+import javax.persistence.*;
+
+@Table(name = "user_info")
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String address;
+
+    private Integer user_id;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return age
+     */
+    public Integer getAge() {
+        return age;
+    }
+
+    /**
+     * @param age
+     */
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    /**
+     * @return address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return user_id
+     */
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    /**
+     * @param user_id
+     */
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", user_id=" + user_id +
+                '}';
+    }
+}
